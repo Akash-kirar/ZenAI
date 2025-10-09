5fb6111de03da69a17a46bf606af1899d2a173af
Akash-kirar <akashkirar541@gmail.com>
2025-10-09 23:02:38 +0530

    next work

---
+++ Backend/server.js
@@ -0,0 +1,15 @@
+import express from "express";
+import "dotenv/config";
+import cors from "cors";
+
+const app= express();
+const PORT = 8080;
+
+
+app.use(express.json());
+app.use(cors());
+
+
+app.listen(PORT, () => {
+console.log(`serer listen on port ${PORT}`);
+});
